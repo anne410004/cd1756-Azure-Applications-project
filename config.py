503f8f4549ade715405cfcdd436ee0291ab2c0d5
Ankita <22051493@kiit.ac.in>
2026-03-13 04:06:00 +0530

    complete project setup

--- config.py
+++ config.py
@@ -5,20 +5,25 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'articlecmsstorage2'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT')
     BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY')
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER')
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'article-cms-server2.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'article-cms-db'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'sqladmin'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
+    SQL_SERVER = os.environ.get('SQL_SERVER')
+    SQL_DATABASE = os.environ.get('SQL_DATABASE')
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME')
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD')
+
+    SQLALCHEMY_DATABASE_URI = (
+        "mssql+pyodbc://{}:{}@{}:1433/{}?driver=ODBC+Driver+17+for+SQL+Server"
+        .format(SQL_USER_NAME, SQL_PASSWORD, SQL_SERVER, SQL_DATABASE)
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    CLIENT_SECRET = os.environ.get('CLIENT_SECRET') or
-    AUTHORITY = "https://login.microsoftonline.com/87e60dd5-c8b5-488a-bcc4-75d6aaed2ba8"
-    CLIENT_ID = "7388d66f-0ce2-49b6-8b2a-0bb6a1934d24"
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+    CLIENT_ID = os.environ.get('CLIENT_ID')
+    AUTHORITY = os.environ.get('AUTHORITY')
 
     REDIRECT_PATH = "/getAToken"
     SCOPE = ["User.Read"]
