eb0b9e4018629c5dd364a6739f6e407a5c2f2bb1
Ankita <22051493@kiit.ac.in>
2026-03-13 15:53:27 +0530

    fix: rename DATABASE_URI to SQLALCHEMY_DATABASE_URI

--- config.py
+++ config.py
@@ -1,28 +1,27 @@
 import os
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config(object):
-
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
-
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT')
     BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY')
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER')
-
     SQL_SERVER = os.environ.get('SQL_SERVER')
     SQL_DATABASE = os.environ.get('SQL_DATABASE')
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME')
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD')
-
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')  # fallback for local
-
+    SQLALCHEMY_DATABASE_URI = (
+        "mssql+pyodbc://{}:{}@{}:1433/{}?driver=ODBC+Driver+17+for+SQL+Server"
+        .format(
+            os.environ.get('SQL_USER_NAME'),
+            os.environ.get('SQL_PASSWORD'),
+            os.environ.get('SQL_SERVER'),
+            os.environ.get('SQL_DATABASE')
+        )
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
     CLIENT_ID = os.environ.get('CLIENT_ID')
     AUTHORITY = os.environ.get('AUTHORITY')
-
     REDIRECT_PATH = "/getAToken"
     SCOPE = ["User.Read"]
     SESSION_TYPE = "filesystem"
