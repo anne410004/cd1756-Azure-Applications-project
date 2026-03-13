16d5f411d1baaf0c4b1d61b46cee70899489d620
Ankita <22051493@kiit.ac.in>
2026-03-13 16:35:39 +0530

    fix hardcoded sql uri

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT')
@@ -9,15 +10,7 @@ class Config(object):
     SQL_DATABASE = os.environ.get('SQL_DATABASE')
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME')
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD')
-    SQLALCHEMY_DATABASE_URI = (
-        "mssql+pyodbc://{}:{}@{}:1433/{}?driver=ODBC+Driver+17+for+SQL+Server"
-        .format(
-            os.environ.get('SQL_USER_NAME'),
-            os.environ.get('SQL_PASSWORD'),
-            os.environ.get('SQL_SERVER'),
-            os.environ.get('SQL_DATABASE')
-        )
-    )
+    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://sqladmin:Youaremyhope%40123@article-cms-server2.database.windows.net:1433/article-cms-db?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes&Connection+Timeout=60"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
     CLIENT_ID = os.environ.get('CLIENT_ID')
